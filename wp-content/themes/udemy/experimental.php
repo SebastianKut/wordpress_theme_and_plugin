49913b83efb03f19513b1c65e3ee86bddf95cfb3
Sebastian Kut <sebastian_kut@yahoo.co.uk>
2020-10-10 17:33:54 +0200

    experimental.php added

---
+++ wp-content/themes/udemy/experimental.php
@@ -0,0 +1,37 @@
+<?php 
+/*
+Template Name: Experimental
+*/
+get_header(); ?>
+
+<!-- Page Title
+============================================= -->
+<section id="page-title">
+  <div class="container clearfix">
+    <h1>Experimental code</h1>
+  </div>
+</section><!-- #page-title end -->
+
+
+<!-- Content
+============================================= -->
+<section id="content">
+
+  <div class="content-wrap">
+
+    
+    <div class="container clearfix">
+
+      <!-- Post Content
+      ============================================= -->
+
+
+      <!-- .postcontent end -->
+
+    </div>
+
+  </div>
+
+</section><!-- #content end -->
+
+<?php get_footer(); ?>
